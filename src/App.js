1fa7e9782c608d05095730a9cccadebd8bebc059
apuzanova <apuzanova@asteriosoft.com>
2022-02-22 21:35:46 +0700

    ShopItemFunc component

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import ShopItemFunc from "./components/ShopItemFunc";
+import './App.css';
+
+function App() {
+    const item = {
+        brand: 'Tiger of Sweden',
+        title: 'Leonard coat',
+        description: 'Minimalistic coat in cotton-blend',
+        descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+        price: 399,
+        currency: '£'
+    }
+
+    return (
+        <div>
+            <ShopItemFunc item={item}/>
+        </div>
+    );
+}
+
+export default App;
